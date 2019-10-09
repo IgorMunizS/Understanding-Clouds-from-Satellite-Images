b1d54d3b3872deceb083277ebecb4f995d45a198
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-09 01:12:35 -0300

    simple data augmentation

--- utils/generator.py
+++ utils/generator.py
@@ -132,26 +132,26 @@ class DataGenerator(keras.utils.Sequence):
         return img
 
     def __random_transform(self, img, masks):
-        composition = albu.Compose([
-            albu.HorizontalFlip(),
-            albu.VerticalFlip(),
-            albu.ShiftScaleRotate(rotate_limit=45, shift_limit=0.15, scale_limit=0.15)
-        ])
-
         # composition = albu.Compose([
-        #                 albu.OneOf([albu.RandomSizedCrop(min_max_height=(self.reshape[0]//2, self.reshape[0]),
-        #                                                  height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
-        #                                                  p=0.5),
-        #                       albu.PadIfNeeded(min_height=self.reshape[0], min_width=self.reshape[1], p=0.5)], p=1),
-        #                 albu.VerticalFlip(p=0.5),
-        #                 albu.HorizontalFlip(),
-        #                 albu.OneOf([
-        #                     albu.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
-        #                     albu.GridDistortion(p=0.5),
-        #                     albu.OpticalDistortion(p=1, distort_limit=2, shift_limit=0.5)
-        #                     ], p=0.8),
-        #                 albu.RandomBrightnessContrast(p=0.8),
-        #                 albu.RandomGamma(p=0.8)])
+        #     albu.HorizontalFlip(),
+        #     albu.VerticalFlip(),
+        #     albu.ShiftScaleRotate(rotate_limit=45, shift_limit=0.15, scale_limit=0.15)
+        # ])
+
+        composition = albu.Compose([
+                        albu.OneOf([albu.RandomSizedCrop(min_max_height=(self.reshape[0]//2, self.reshape[0]),
+                                                         height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
+                                                         p=0.5),
+                              albu.PadIfNeeded(min_height=self.reshape[0], min_width=self.reshape[1], p=0.5)], p=1),
+                        albu.VerticalFlip(p=0.5),
+                        albu.HorizontalFlip(),
+                        albu.OneOf([
+                            albu.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+                            albu.GridDistortion(p=0.5),
+                            albu.OpticalDistortion(p=1, distort_limit=2, shift_limit=0.5)
+                            ], p=0.8),
+                        albu.RandomBrightnessContrast(p=0.8),
+                        albu.RandomGamma(p=0.8)])
 
 
         composed = composition(image=img, mask=masks)
