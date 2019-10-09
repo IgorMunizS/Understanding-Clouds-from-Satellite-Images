e3109e37dc7ad7a36872062cc561ac0063e54d8c
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-08 23:31:11 -0300

    add randomcrop to data augmentation

--- utils/generator.py
+++ utils/generator.py
@@ -140,7 +140,7 @@ class DataGenerator(keras.utils.Sequence):
 
         composition = albu.Compose([
                         albu.RandomSizedCrop(min_max_height=(0, self.reshape[0]),
-                                                         height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
+                                                         height=320, width=480, w2h_ratio=1.5,
                                                          p=0.5),
                         albu.VerticalFlip(p=0.5),
                         albu.RandomRotate90(p=0.5),
