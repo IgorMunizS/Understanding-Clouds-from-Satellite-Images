d1fb804f876e4411a1f5b92a79a64887b46cddf4
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-10 14:00:55 -0300

    add callback for postprocess

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -28,29 +28,47 @@ def post_process_callback(val_predict,shape):
 
 class ValPosprocess(callbacks.Callback):
 
-    def set_shape(self,shape):
+    def __init__(self, val_data, batch_size=20,shape=(320,480)):
+        super().__init__()
+        self.validation_data = val_data
+        self.batch_size = batch_size
         self.shape = shape
 
+
     def on_epoch_end(self, epoch, logs={}):
-        val_targ = 0
-        val_predict = 0
 
-        # 5.4.1 For each validation batch
-        for batch_index in range(0, len(self.validation_data)):
-            # 5.4.1.1 Get the batch target values
-            temp_targ = self.validation_data[batch_index][1]
-            # 5.4.1.2 Get the batch prediction values
-            temp_predict = (np.asarray(self.model.predict(
-                                self.validation_data[batch_index][0]))).round()
-            # 5.4.1.3 Append them to the corresponding output objects
-            if(batch_index == 0):
-                val_targ = temp_targ
-                val_predict = temp_predict
-            else:
-                val_targ = np.vstack((val_targ, temp_targ))
-                val_predict = np.vstack((val_predict, temp_predict))
+        batches = len(self.validation_data)
+        total = batches * self.batch_size
 
-        val_predict_posprocess = post_process_callback(val_predict,self.shape)
+        val_pred = np.zeros((total, 1))
+        val_true = np.zeros((total))
+
+        for batch in range(batches):
+            xVal, yVal = next(self.validation_data)
+            val_pred[batch * self.batch_size: (batch + 1) * self.batch_size] = np.asarray(
+                self.model.predict(xVal)).round()
+            val_true[batch * self.batch_size: (batch + 1) * self.batch_size] = yVal
+
+        val_pred = np.squeeze(val_pred)
+
+
+
+        # # 5.4.1 For each validation batch
+        # for batch_index in range(0, len(self.validation_data)):
+        #     # 5.4.1.1 Get the batch target values
+        #     temp_targ = self.validation_data[batch_index][1]
+        #     # 5.4.1.2 Get the batch prediction values
+        #     temp_predict = (np.asarray(self.model.predict(
+        #                         self.validation_data[batch_index][0]))).round()
+        #     # 5.4.1.3 Append them to the corresponding output objects
+        #     if(batch_index == 0):
+        #         val_targ = temp_targ
+        #         val_predict = temp_predict
+        #     else:
+        #         val_targ = np.vstack((val_targ, temp_targ))
+        #         val_predict = np.vstack((val_predict, temp_predict))
+
+        val_predict_posprocess = post_process_callback(val_pred,self.shape)
 
 
         dice_coef_posprocess = round(dice_coef(val_predict_posprocess, val_predict), 4)
