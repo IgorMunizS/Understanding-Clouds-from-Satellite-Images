742fafebd3db0a0342833a3149aaef4fbcc6fad6
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-10 01:40:09 -0300

    fix loss

--- utils/losses.py
+++ utils/losses.py
@@ -1,27 +1,28 @@
 import keras.backend as K
+from keras.losses import binary_crossentropy
 
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+# def bce_dice_loss(y_true, y_pred):
+#     return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
 
 # def dice_coef(y_true, y_pred, smooth=1.0):
 #     y_true_f = K.flatten(y_true)
 #     y_pred_f = K.flatten(y_pred)
 #     intersection = K.sum(y_true_f * y_pred_f)
 #     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-
-def dice_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred = K.cast(y_pred, 'float32')
-    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
-    intersection = y_true_f * y_pred_f
-    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
-    return score
-
-def binary_crossentropy(y, p):
-    return K.mean(K.binary_crossentropy(y, p))
-
-
-def dice_coef_loss(y_true, y_pred):
-    return 1 - dice_coef(y_true, y_pred)
-
 
 def dice_coef_loss_bce(y_true, y_pred, dice=0.5, bce=0.5):
     return binary_crossentropy(y_true, y_pred) * bce + dice_coef_loss(y_true, y_pred) * dice
