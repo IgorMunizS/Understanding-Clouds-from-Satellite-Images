ce324d224612421a7f904944d1f66abb73e8df7b
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-12 15:34:38 -0300

    new prediction with emsemble

--- utils/losses.py
+++ utils/losses.py
@@ -24,7 +24,7 @@ def binary_crossentropy_smoothed(y_true, y_pred):
         y_true,
         y_pred,
         from_logits=False,
-        label_smoothing=0.2
+        label_smoothing=0.1
     )
     return loss
 
