ba0e1f7e869dc5e6624d27074648cff6ec7ee764
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-12 21:08:57 -0300

    combo loss

--- utils/losses.py
+++ utils/losses.py
@@ -51,8 +51,14 @@ def focal_loss(y_true, y_pred):
     alpha = 0.25
     pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
     pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-    return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean(
-        (1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+
+    epsilon = K.epsilon()
+    # clip to prevent NaN's and Inf's
+    pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
+    pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
+
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) \
+           - K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 
 
 def combo_loss(y_true, y_pred,dice=0.2, bce=0.4, focal=0.4):
