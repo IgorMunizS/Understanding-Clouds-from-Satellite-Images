26fe9ff633cc3956c6eb85f44ca7586a0f4608fd
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-13 09:39:21 -0300

    new model xnet

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import segmentation_models as sm
+import UNetPlusPlus.segmentation_models as smx
 
 def get_model(model,BACKBONE,opt,loss,metric,shape):
     h,w = shape
@@ -14,6 +15,15 @@ def get_model(model,BACKBONE,opt,loss,metric,shape):
 
     elif model == 'unet':
         model = sm.Unet(
+            BACKBONE,
+            classes=4,
+            input_shape=(h, w, 3),
+            activation='sigmoid'
+        )
+        model.compile(optimizer=opt, loss=loss, metrics=[metric])
+
+    elif model == 'xnet':
+        model = smx.Xnet(
             BACKBONE,
             classes=4,
             input_shape=(h, w, 3),
