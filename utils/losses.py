55391f7d70c22f2d4181a52187a5ca15d2d79d74
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-16 22:52:45 -0300

    average emsembles with postprocess

--- utils/losses.py
+++ utils/losses.py
@@ -152,8 +152,9 @@ def lovasz_loss(y_true, y_pred):
 
 
 def np_dice_coef(y_true, y_pred):
+    smooth = 1.
     y_true_f = y_true.flatten()
     y_pred_f = y_pred.flatten()
     intersection = np.sum(y_true_f * y_pred_f)
-    return ( (2. * intersection) /
-             (np.sum(y_true_f) + np.sum(y_pred_f)) )
+    return ( (2. * intersection + smooth) /
+             (np.sum(y_true_f) + np.sum(y_pred_f) + smooth) )
