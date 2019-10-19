db3f6b72d194a5219897ab0dac4ccbe88dbe1979
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-19 01:22:17 -0300

    new train strategy

--- utils/generator.py
+++ utils/generator.py
@@ -156,12 +156,11 @@ class DataGenerator(keras.utils.Sequence):
                         #                                  p=0.5),
                         #       albu.PadIfNeeded(min_height=self.reshape[0], min_width=self.reshape[1], p=0.5)], p=0.3),
                         albu.RandomSizedCrop(min_max_height=(self.reshape[0] // 2, self.reshape[0]),
-                                                                   height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
+                                                                   height=self.reshape[0], width=self.reshape[1], w2h_ratio=1,
                                                                    p=0.3),
-                        albu.Solarize(),
                         albu.HorizontalFlip(),
                         albu.VerticalFlip(),
-                        albu.ShiftScaleRotate(),
+                        albu.ShiftScaleRotate(rotate_limit=45, shift_limit=0.15, scale_limit=0.15),
                         albu.OneOf([
                             albu.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
                             albu.GridDistortion(p=0.5),
@@ -171,6 +170,7 @@ class DataGenerator(keras.utils.Sequence):
                             albu.RandomContrast(),
                             albu.RandomGamma(),
                             albu.RandomBrightness(),
+                            albu.Solarize()
                         ], p=0.3)
         ], p=1)
 
