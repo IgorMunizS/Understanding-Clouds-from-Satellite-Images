ddfe82a75f40f92441a648a08308a4ada1ba3448
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-21 13:46:19 -0300

    add deeplab model

--- utils/generator.py
+++ utils/generator.py
@@ -5,8 +5,9 @@ import segmentation_models as sm
 import cv2
 from utils.utils import build_masks,np_resize
 from skimage.exposure import adjust_gamma
+import tensorflow as tf
 
-class DataGenerator(keras.utils.Sequence):
+class DataGenerator(tf.python.keras.utils.data_utils.Sequence):
     'Generates data for Keras'
 
     def __init__(self, list_IDs, df, target_df=None, mode='fit',
