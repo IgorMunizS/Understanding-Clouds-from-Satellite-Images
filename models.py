b363a8fd39509bd35304fa87fa09d3161a5e23fb
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-21 13:38:43 -0300

    add deeplab model

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import segmentation_models as sm
 import UNetPlusPlus.segmentation_models as smx
 from utils.jpu import JPU_DeepLab
-from tensorflow.keras.optimizers import Nadam
+from deeplabv3.model import Deeplabv3
 
 def get_model(model,BACKBONE,opt,loss,metric,shape):
     h,w = shape
@@ -36,6 +36,12 @@ def get_model(model,BACKBONE,opt,loss,metric,shape):
     elif model == 'jpu':
         model = JPU_DeepLab(h,w,4)
         model.compile(optimizer=opt, loss=loss, metrics=[metric])
+
+    elif model == 'deeplab':
+        model = Deeplabv3(weights=None, input_shape=(h,w,4), classes=4, backbone='xception',
+              alpha=1., activation='sigmoid')
+        model.compile(optimizer=opt, loss=loss, metrics=[metric])
+
     else:
         raise ValueError('Unknown network ' + model)
 
