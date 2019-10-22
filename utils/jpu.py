e086478d63ba80abdeb4ea2ee8574f8e55954e49
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-22 19:39:49 -0300

    add new loss

--- utils/jpu.py
+++ utils/jpu.py
@@ -38,26 +38,26 @@ def JPU(endpoints: list, out_channels=512):
         if i != 1:
             h_t, w_t = endpoints[i].shape.as_list()[1:3]
             scale = (h // h_t, w // w_t)
-            endpoints[i] = KL.UpSampling2D(
+            endpoints[i] = tf.keras.layers.UpSampling2D(
                 size=scale, interpolation='bilinear')(endpoints[i])
-    yc = KL.Concatenate(axis=-1)(endpoints[1:])
+    yc = tf.keras.layers.Concatenate(axis=-1)(endpoints[1:])
     ym = []
     for rate in [1, 2, 4, 8]:
         ym.append(sepconv_block(yc, 512, 3, dilation_rate=rate))
-    y = KL.Concatenate(axis=-1)(ym)
+    y = tf.keras.layers.Concatenate(axis=-1)(ym)
     return endpoints, y
 
 
 def ASPP(tensor):
     dims = tensor.shape.as_list()
 
-    y_pool = KL.AveragePooling2D(pool_size=(
+    y_pool = tf.keras.layers.AveragePooling2D(pool_size=(
         dims[1], dims[2]), name='average_pooling')(tensor)
     y_pool = conv_block(y_pool, num_filters=256, kernel_size=1)
 
     h_t, w_t = y_pool.shape.as_list()[1:3]
     scale = dims[1] // h_t, dims[2] // w_t
-    y_pool = KL.UpSampling2D(
+    y_pool = tf.keras.layers.UpSampling2D(
         size=scale, interpolation='bilinear')(y_pool)
 
     y_1 = conv_block(tensor, num_filters=256, kernel_size=1, dilation_rate=1)
@@ -68,7 +68,7 @@ def ASPP(tensor):
     y_18 = conv_block(tensor, num_filters=256, kernel_size=3, dilation_rate=18)
     y_18.set_shape([None, dims[1], dims[2], 256])
 
-    y = KL.Concatenate(axis=-1)([y_pool, y_1, y_6, y_12, y_18])
+    y = tf.keras.layers.Concatenate(axis=-1)([y_pool, y_1, y_6, y_12, y_18])
     y = conv_block(y, num_filters=256, kernel_size=1)
     return y
 
@@ -86,19 +86,19 @@ def JPU_DeepLab(img_height=1024, img_width=1024, nclasses=19):
     x_a = ASPP(image_features)
     h_t, w_t = x_a.shape.as_list()[1:3]
     scale = (img_height / 4) // h_t, (img_width / 4) // w_t
-    x_a = KL.UpSampling2D(
+    x_a = tf.keras.layers.UpSampling2D(
         size=scale, interpolation='bilinear')(x_a)
 
     x_b = base_model.get_layer('conv2_block3_out').output
     x_b = conv_block(x_b, num_filters=48, kernel_size=1)
 
-    x = KL.Concatenate(axis=-1)([x_a, x_b])
+    x = tf.keras.layers.Concatenate(axis=-1)([x_a, x_b])
     x = conv_block(x, num_filters=256, kernel_size=3)
     x = conv_block(x, num_filters=256, kernel_size=3)
     h_t, w_t = x.shape.as_list()[1:3]
     scale = img_height // h_t, img_width // w_t
-    x = KL.UpSampling2D(size=scale, interpolation='bilinear')(x)
+    x = tf.keras.layers.UpSampling2D(size=scale, interpolation='bilinear')(x)
 
-    x = KL.Conv2D(nclasses, (1, 1), name='output_layer')(x)
+    x = tf.keras.layers.Conv2D(nclasses, (1, 1), name='output_layer')(x)
     model = Model(inputs=base_model.input, outputs=x, name='JPU')
     return model
