94a6fda26060746a86fe90357c9507f9a38ebb16
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-27 23:37:08 -0300

    add fixhshape in prediction

--- train.py
+++ train.py
@@ -63,12 +63,12 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
                 backbone=backbone
             )
 
-            opt = RAdam(lr=0.0003)
-            # opt = Nadam(lr=0.0003)
+            # opt = RAdam(lr=0.0003)
+            opt = Nadam(lr=0.0003)
             # opt = AdamAccumulate(lr=0.0003, accum_iters=8)
-            optimizer = GradientAccumulation(opt, accumulation_steps=4)
+            # optimizer = GradientAccumulation(opt, accumulation_steps=4)
 
-            model = get_model(smmodel,backbone,optimizer,dice_coef_loss_bce,dice_coef,shape)
+            model = get_model(smmodel,backbone,opt,dice_coef_loss_bce,dice_coef,shape)
             swa = SWA('../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + '_swa.h5', epochs - 3)
 
 
