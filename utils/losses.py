1ee794c8de3bb4bf2fb41531497251febe26dff9
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-28 17:06:13 -0300

    fix predict

--- utils/losses.py
+++ utils/losses.py
@@ -16,8 +16,7 @@ def dice_coef(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     smooth = 1e-5
     y_true_f = K.flatten(y_true)
-    y_pred = K.cast(y_pred, 'float32')
-    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    y_pred_f = K.flatten(y_pred)
     intersection = y_true_f * y_pred_f
     score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return 1. - score
