c861ec04e1ff59149ffbbd61535b1f7d799db5eb
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-29 13:21:11 -0300

    fix predict

--- utils/generator.py
+++ utils/generator.py
@@ -155,25 +155,28 @@ class DataGenerator(keras.utils.Sequence):
                         #                                  height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
                         #                                  p=0.5),
                         #       albu.PadIfNeeded(min_height=self.reshape[0], min_width=self.reshape[1], p=0.5)], p=0.3),
-                        albu.OneOf([
-                            albu.RandomSizedCrop(min_max_height=(self.reshape[0] // 2, self.reshape[0]),
-                                                 height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
-                                                 p=0.5),
-                            albu.CropNonEmptyMaskIfExists(self.reshape[0], self.reshape[1], p=0.3)
-                        ], p=0.3),
+                        # albu.OneOf([
+                        #     albu.RandomSizedCrop(min_max_height=(self.reshape[0] // 2, self.reshape[0]),
+                        #                          height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
+                        #                          p=0.5),
+                        #     albu.CropNonEmptyMaskIfExists(self.reshape[0], self.reshape[1], p=0.3)
+                        # ], p=0.3),
+                        albu.RandomSizedCrop(min_max_height=(self.reshape[0] // 2, self.reshape[0]),
+                                 height=self.reshape[0], width=self.reshape[1], w2h_ratio=1.5,
+                                 p=0.5),
                         albu.HorizontalFlip(),
                         albu.VerticalFlip(),
                         albu.ShiftScaleRotate(rotate_limit=45, shift_limit=0.15, scale_limit=0.15),
                         albu.OneOf([
                             albu.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
                             albu.GridDistortion(p=0.5),
-                            albu.OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5)
+                            # albu.OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5)
                             ], p=0.3),
                         albu.OneOf([
                             albu.RandomContrast(),
                             albu.RandomGamma(),
                             albu.RandomBrightness(),
-                            albu.Solarize()
+                            # albu.Solarize()
                         ], p=0.5)
         ], p=1)
 
