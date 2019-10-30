2b6359286bd1261efd22ae8413ac5722b457a342
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-30 18:22:23 -0300

    add background to masks

--- utils/losses.py
+++ utils/losses.py
@@ -6,8 +6,8 @@ import segmentation_models as sm
 
 
 def dice_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred = K.cast(y_pred, 'float32')
+    y_true_f = K.flatten(y_true[:,:,:,:4])
+    y_pred = K.cast(y_pred[:,:,:,:4], 'float32')
     y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
     intersection = y_true_f * y_pred_f
     score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
