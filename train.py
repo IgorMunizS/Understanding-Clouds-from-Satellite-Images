f8d38378ae7ab57f692f6d9317d4302a885481a7
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-30 15:10:34 -0300

    old dice metric for comparison

--- train.py
+++ train.py
@@ -64,8 +64,8 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
 
             # opt = RAdam(lr=0.0003)
             # opt = Nadam(lr=0.0003)
-            opt = Adam(lr=0.0003)
-            # opt = AdamAccumulate(lr=0.0003, accum_iters=8)
+            # opt = Adam(lr=0.0003)
+            opt = AdamAccumulate(lr=0.0003, accum_iters=4)
             # optimizer = GradientAccumulation(opt, accumulation_steps=4)
 
             dice_focal_loss = sm_loss()
