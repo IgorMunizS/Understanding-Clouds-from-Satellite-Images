d064a7686f9c9c82614c1b063b3e2a28ffb652d9
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-30 00:15:03 -0300

    fix predict

--- train.py
+++ train.py
@@ -63,8 +63,8 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
             )
 
             # opt = RAdam(lr=0.0003)
-            opt = Nadam(lr=0.0003)
-            # opt = Adam(lr=0.0003)
+            # opt = Nadam(lr=0.0003)
+            opt = Adam(lr=0.0003)
             # opt = AdamAccumulate(lr=0.0003, accum_iters=8)
             # optimizer = GradientAccumulation(opt, accumulation_steps=4)
 
@@ -75,8 +75,8 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
             filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + '.h5'
             ckp = ModelCheckpoint(filepath, monitor='val_f1-score', verbose=1, save_best_only=True, mode='max',
                                          save_weights_only=True)
-            es = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=1, mode='min')
-            rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, verbose=1, mode='min', min_delta=0.0001)
+            es = EarlyStopping(monitor='val_f1-score', min_delta=0.0001, patience=5, verbose=1, mode='max')
+            rlr = ReduceLROnPlateau(monitor='val_f1-score', factor=0.2, patience=3, verbose=1, mode='max', min_delta=0.0001)
 
             history = model.fit_generator(
                 train_generator,
