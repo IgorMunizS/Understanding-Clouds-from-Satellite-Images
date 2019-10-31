e49df3b6baa04def17cb75c70345bc1e0953f6d9
Muniz Soares, Igor <imunizs@indracompany.com>
2019-10-31 12:01:28 -0300

    add background to masks

--- train.py
+++ train.py
@@ -72,8 +72,8 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
             # dice_focal_loss = sm_loss()
             # dice_metric = jaccard()
 
-            metrics = [dice_coef,dice_coef_fish,dice_coef_flower,dice_coef_gravel,dice_coef_sugar]
-            model = get_model(smmodel,backbone,opt,dice_coef_loss_bce,metrics,shape)
+            # metrics = [dice_coef,dice_coef_fish,dice_coef_flower,dice_coef_gravel,dice_coef_sugar]
+            model = get_model(smmodel,backbone,opt,dice_coef_loss_bce,[dice_coef],shape)
 
             filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + '.h5'
             ckp = ModelCheckpoint(filepath, monitor='val_dice_coef', verbose=1, save_best_only=True, mode='max',
