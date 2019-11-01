b74018d94d4485c143f3ba1b78729967a0433933
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-01 13:39:36 -0300

    train per class

--- train.py
+++ train.py
@@ -32,10 +32,10 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
 
 
     for n_fold, (train_indices, val_indices) in enumerate(skf.split(mask_count_df.index)):
-        # train_indices, _ = ros.fit_resample(train_indices.reshape(-1, 1),
-        #                                            mask_count_df[mask_count_df.index.isin(train_indices)]['hasMask'])
-        #
-        # train_indices = list(itertools.chain.from_iterable(train_indices))
+        train_indices, _ = ros.fit_resample(train_indices.reshape(-1, 1),
+                                                   mask_count_df[mask_count_df.index.isin(train_indices)]['hasMask'])
+
+        train_indices = list(itertools.chain.from_iterable(train_indices))
 
         if n_fold >= nfold:
             print('Training fold number ',str(n_fold))
@@ -71,7 +71,8 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
             # opt = RAdam(lr=0.0003)
             # opt = Nadam(lr=0.0003)
             # opt = RMSprop(lr=0.0003)
-            opt = AdamAccumulate(lr=0.0003, accum_iters=4)
+            accum_iters = 64 // batch_size
+            opt = AdamAccumulate(lr=0.0003, accum_iters=accum_iters)
             # optimizer = GradientAccumulation(opt, accumulation_steps=4)
 
             dice_focal_loss = sm_loss()
