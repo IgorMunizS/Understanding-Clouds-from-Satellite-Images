9f0897b1eddb8482dbb80ad013ba49edc99835bf
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-03 01:12:45 -0300

    train per class

--- evaluation.py
+++ evaluation.py
@@ -174,16 +174,16 @@ def search(val_file,shape,fixshape=False, emsemble=False):
     print(oof_data.shape)
     print(oof_predicted_data.shape)
 
-    for x,img in tqdm(enumerate(oof_predicted_data)):
-        for k in range(4):
-            im_layer = img[:,:,k]
-            max_col = np.max(im_layer, axis=1)
-            max_row = np.max(im_layer, axis=0)
-            mat = [max_col[i] + max_row[j] for i in range(h) for j in range(w)]
-            mat = np.reshape(mat, (h,w))
-            im_layer = 0.7*im_layer + 0.3*mat
-
-            oof_predicted_data[x,:,:,k] = im_layer
+    # for x,img in tqdm(enumerate(oof_predicted_data)):
+    #     for k in range(4):
+    #         im_layer = img[:,:,k]
+    #         max_col = np.max(im_layer, axis=1)
+    #         max_row = np.max(im_layer, axis=0)
+    #         mat = [max_col[i] + max_row[j] for i in range(h) for j in range(w)]
+    #         mat = np.reshape(mat, (h,w))
+    #         im_layer = 0.7*im_layer + 0.3*mat
+    #
+    #         oof_predicted_data[x,:,:,k] = im_layer
 
 
     now = time.time()
