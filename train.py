818667b469eaffc7703d544e4566ba1eb12a5961
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-08 01:37:16 -0300

    eval model for each class

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
 
             metrics = [dice_coef,dice_coef_fish,dice_coef_flower,dice_coef_gravel,dice_coef_sugar]
             model = get_model(smmodel,backbone,opt,dice_coef_loss_bce,[dice_coef],shape)
-            filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + '_' + str(classe)
+            filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold)
 
             ckp = ModelCheckpoint(filepath + '.h5', monitor='val_dice_coef', verbose=1, save_best_only=True, mode='max',
                                          save_weights_only=True)
