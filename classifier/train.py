223c9d9aa10938d73d452c24dc899b6d8db93d9f
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-08 11:03:36 -0300

    add classifier training

--- classifier/train.py
+++ classifier/train.py
@@ -25,9 +25,17 @@ def train(cls_model='b2', shape=(320,320)):
     for n_fold, (train_indices, val_indices) in enumerate(kfold.split(train_df['Image'].values, train_df['Class'].map(lambda x: str(sorted(list(x)))))):
         train_imgs = train_df['Image'].values[train_indices]
         val_imgs = train_df['Image'].values[val_indices]
-        data_generator_train = DataGenenerator(train_imgs, augmentation=albumentations_train, img_2_ohe_vector=img_2_vector)
-        data_generator_train_eval = DataGenenerator(train_imgs, shuffle=False, img_2_ohe_vector=img_2_vector)
-        data_generator_val = DataGenenerator(val_imgs, shuffle=False, img_2_ohe_vector=img_2_vector)
+        data_generator_train = DataGenenerator(train_imgs, augmentation=albumentations_train,
+                                               resized_height=shape[0], resized_width=shape[0],
+                                               img_2_ohe_vector=img_2_vector)
+
+        data_generator_train_eval = DataGenenerator(train_imgs, shuffle=False,
+                                                    resized_height=shape[0], resized_width=shape[0],
+                                                    img_2_ohe_vector=img_2_vector)
+
+        data_generator_val = DataGenenerator(val_imgs, shuffle=False,
+                                             resized_height=shape[0], resized_width=shape[0],
+                                             img_2_ohe_vector=img_2_vector)
 
         model.compile(optimizer=RAdam(warmup_proportion=0.1, min_lr=1e-5), loss='categorical_crossentropy',
                       metrics=['accuracy'])
