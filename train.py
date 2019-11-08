feb4836fbdc46bbe75e0e78188f4c2b3f57769e0
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-08 01:35:08 -0300

    eval model for each class

--- train.py
+++ train.py
@@ -34,72 +34,72 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
         #                                            mask_count_df[mask_count_df.index.isin(train_indices)]['hasMask'])
         #
         # train_indices = list(itertools.chain.from_iterable(train_indices))
-        val_images = mask_count_df[mask_count_df.index.isin(val_indices)]['ImageId'].tolist()
-        train_images = mask_count_df[mask_count_df.index.isin(train_indices)]['ImageId'].tolist()
+        # val_images = mask_count_df[mask_count_df.index.isin(val_indices)]['ImageId'].tolist()
+        # train_images = mask_count_df[mask_count_df.index.isin(train_indices)]['ImageId'].tolist()
+        #
+        # for classe in ['Fish','Flower','Gravel','Sugar']:
+        #     train_df, mask_count_df = get_data_preprocessed(pseudo_label, classe=classe)
+        #     val_indices = mask_count_df[mask_count_df['ImageId'].isin(val_images)].index
+        #     train_indices = mask_count_df[mask_count_df['ImageId'].isin(train_images)].index
+        #     print('Training class ', classe)
+        if n_fold >= nfold:
+            print('Training fold number ',str(n_fold))
 
-        for classe in ['Fish','Flower','Gravel','Sugar']:
-            train_df, mask_count_df = get_data_preprocessed(pseudo_label, classe=classe)
-            val_indices = mask_count_df[mask_count_df['ImageId'].isin(val_images)].index
-            train_indices = mask_count_df[mask_count_df['ImageId'].isin(train_images)].index
-            print('Training class ', classe)
-            if n_fold >= nfold:
-                print('Training fold number ',str(n_fold))
 
+            train_generator = DataGenerator(
+                train_indices,
+                df=mask_count_df,
+                target_df=train_df,
+                batch_size=batch_size,
+                reshape=shape,
+                augment=True,
+                n_channels=3,
+                n_classes=n_classes,
+                backbone=backbone,
+                randomcrop=False,
+            )
 
-                train_generator = DataGenerator(
-                    train_indices,
-                    df=mask_count_df,
-                    target_df=train_df,
-                    batch_size=batch_size,
-                    reshape=shape,
-                    augment=True,
-                    n_channels=3,
-                    n_classes=n_classes,
-                    backbone=backbone,
-                    randomcrop=False,
-                )
+            val_generator = DataGenerator(
+                val_indices,
+                df=mask_count_df,
+                target_df=train_df,
+                batch_size=batch_size,
+                reshape=shape,
+                augment=False,
+                n_channels=3,
+                n_classes=n_classes,
+                backbone=backbone,
+                randomcrop=False,
+            )
 
-                val_generator = DataGenerator(
-                    val_indices,
-                    df=mask_count_df,
-                    target_df=train_df,
-                    batch_size=batch_size,
-                    reshape=shape,
-                    augment=False,
-                    n_channels=3,
-                    n_classes=n_classes,
-                    backbone=backbone,
-                    randomcrop=False,
-                )
+            # opt = RAdam(lr=0.0003)
+            opt = Nadam(lr=0.0003)
+            # opt = RMSprop(lr=0.0003)
+            # accum_iters = 64 // batch_size
+            # opt = AdamAccumulate(lr=0.0003, accum_iters=accum_iters)
+            # optimizer = GradientAccumulation(opt, accumulation_steps=4)
 
-                # opt = RAdam(lr=0.0003)
-                opt = Nadam(lr=0.0003)
-                # opt = RMSprop(lr=0.0003)
-                # accum_iters = 64 // batch_size
-                # opt = AdamAccumulate(lr=0.0003, accum_iters=accum_iters)
-                # optimizer = GradientAccumulation(opt, accumulation_steps=4)
+            dice_focal_loss = sm_loss()
+            dice_metric = jaccard()
 
-                dice_focal_loss = sm_loss()
-                dice_metric = jaccard()
+            metrics = [dice_coef,dice_coef_fish,dice_coef_flower,dice_coef_gravel,dice_coef_sugar]
+            model = get_model(smmodel,backbone,opt,dice_coef_loss_bce,[dice_coef],shape)
+            filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + '_' + str(classe)
 
-                metrics = [dice_coef,dice_coef_fish,dice_coef_flower,dice_coef_gravel,dice_coef_sugar]
-                model = get_model(smmodel,backbone,opt,dice_coef_loss_bce,[dice_coef],shape)
-                filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + '_' + str(classe)
+            ckp = ModelCheckpoint(filepath + '.h5', monitor='val_dice_coef', verbose=1, save_best_only=True, mode='max',
+                                         save_weights_only=True)
 
-                ckp = ModelCheckpoint(filepath + '.h5', monitor='val_dice_coef', verbose=1, save_best_only=True, mode='max',
-                                             save_weights_only=True)
+            es = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=1, mode='min')
+            rlr = ReduceLROnPlateau(monitor='val_dice_coef', factor=0.2, patience=3, verbose=1, mode='max', min_delta=0.0001)
 
-                es = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=1, mode='min')
-                rlr = ReduceLROnPlateau(monitor='val_dice_coef', factor=0.2, patience=3, verbose=1, mode='max', min_delta=0.0001)
-
-                history = model.fit_generator(
-                    train_generator,
-                    validation_data=val_generator,
-                    callbacks=[ckp, rlr, es],
-                    epochs=epochs,
-                    use_multiprocessing=True,
-                    workers=42
-                )
+            history = model.fit_generator(
+                train_generator,
+                validation_data=val_generator,
+                callbacks=[ckp, rlr, es],
+                epochs=epochs,
+                use_multiprocessing=True,
+                workers=42
+            )
 
 
 def parse_args(args):
