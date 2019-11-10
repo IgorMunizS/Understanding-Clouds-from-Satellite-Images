9f6802557a0dd21a4777d759bc4c2e61bd81e3d3
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-10 10:06:44 -0300

    train with combo loss penalizing fp

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
 
             metrics = [dice_coef,dice_coef_fish,dice_coef_flower,dice_coef_gravel,dice_coef_sugar]
             model = get_model(smmodel,backbone,opt,combo_loss_init,[dice_coef],freeze_encoder=False,batchnormalization=False)
-            filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold)
+            filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + 'test'
 
             ckp = ModelCheckpoint(filepath + '.h5', monitor='val_dice_coef', verbose=1, save_best_only=True, mode='max',
                                          save_weights_only=True)
