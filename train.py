0861bfe6d0d25a07faf1060285a6cb304e072d7d
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-11 09:54:26 -0300

    change training loss

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ def train(smmodel,backbone,batch_size,shape=(320,480),nfold=0,pseudo_label=None)
             dice_metric = jaccard()
 
             metrics = [dice_coef,dice_coef_fish,dice_coef_flower,dice_coef_gravel,dice_coef_sugar]
-            model = get_model(smmodel,backbone,opt,combo_loss_init,[dice_coef])
+            model = get_model(smmodel,backbone,opt,dice_coef_loss_bce,[dice_coef])
             filepath = '../models/best_' + str(smmodel) + '_' + str(backbone) + '_' + str(n_fold) + 'test'
 
             ckp = ModelCheckpoint(filepath + '.h5', monitor='val_dice_coef', verbose=1, save_best_only=True, mode='max',
