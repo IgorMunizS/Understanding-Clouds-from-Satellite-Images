7e2876302d137c15bed13370aad465797cebcfce
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-15 07:38:17 -0300

    evaluate multimodel

--- train.py
+++ train.py
@@ -127,7 +127,7 @@ def parse_args(args):
     parser.add_argument('--model', help='Segmentation model', default='unet')
     parser.add_argument('--backbone', help='Model backbone', default='resnet34', type=str)
     parser.add_argument('--batch_size', default=32, type=int)
-    parser.add_argument('--shape', help='Shape of resized images', default=(320,480), type=tuple)
+    parser.add_argument('--shape', help='Shape of resized images', nargs='+', default=[320, 480], type=int)
     parser.add_argument('--n_fold', help='Number of fold to start training', default=0, type=int)
     parser.add_argument('--pseudo_label', help='Add extra data from test', default=None, type=str)
     return parser.parse_args(args)
@@ -135,6 +135,6 @@ def parse_args(args):
 if __name__ == '__main__':
     args = sys.argv[1:]
     args = parse_args(args)
+    h,w = args.shape
 
-
-    train(args.model,args.backbone,args.batch_size,args.shape,args.n_fold,args.pseudo_label)
+    train(args.model,args.backbone,args.batch_size,(h,w),args.n_fold,args.pseudo_label)
