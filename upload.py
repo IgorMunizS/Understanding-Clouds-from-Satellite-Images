7010d410d95d67e03fe306007848aa1fc2ebae26
Muniz Soares, Igor <imunizs@indracompany.com>
2019-11-15 08:16:05 -0300

    upload model

---
+++ upload.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+import argparse
+import sys
+
+def save_blend_int(val_file,type='oof'):
+
+    for i, file in enumerate(os.listdir(val_file)):
+        if i == 0:
+            oof_predicted_data = np.load(val_file + file)
+        else:
+            oof_predicted_data += np.load(val_file + file)
+
+    oof_predicted_data = (oof_predicted_data*100).astype(np.int8)
+    np.save(val_file + '_blend_' + type +'.npy', oof_predicted_data)
+
+def parse_args(args):
+    """ Parse the arguments.
+    """
+    parser = argparse.ArgumentParser(description='Predict script')
+
+
+
+    parser.add_argument('--val_file', help='val file to search', default=None, type=str)
+    parser.add_argument('--type', help='apply shape convex or not', default='oof', type=str)
+
+
+
+
+    return parser.parse_args(args)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    args = parse_args(args)
+
+    save_blend_int(args.val_file,args.type)
